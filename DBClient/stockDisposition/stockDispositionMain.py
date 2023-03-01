0c34fc5d9e2b6e33174fd411dc8cc44b1bcb5b84
bjm7701 <bjm7701@naver.com>
2023-03-01 23:38:32 +0900

    자기주식 처분결정 이벤트 db 적재 및 푸시 기능 추가

---
+++ DBClient/stockDisposition/stockDispositionMain.py
@@ -0,0 +1,87 @@
+import sqlalchemy.exc
+from fastapi import APIRouter, Request
+import sys,os
+sys.path.append("../DBClient") #상위 경로를 현재 경로에 넣어 declaration 파일 임포트 가능
+from databaseCmn import engineConn
+from stockDisposition.models import StockDispositionInfo
+from pydantic import BaseModel
+from loguru import logger
+from sqlalchemy.sql import text
+from sqlalchemy import Column, Table, TEXT, BIGINT, MetaData, insert, cast, String
+
+stockDispositionRouter = APIRouter()
+conn = engineConn()
+#session = conn.sessionmaker()
+metadata = MetaData()
+tbStockDispositionInfo =  Table('TB_STOCK_DISPOSITION_INFO',metadata,
+    Column('rcept_no',BIGINT, nullable=False,primary_key=True,unique=True),
+    Column('corp_cls',TEXT, nullable=False),
+    Column('corp_code',TEXT, nullable=False),
+    Column('corp_name',TEXT, nullable=False),
+    Column('dppln_stk_ostk',TEXT, nullable=False),
+    Column('dppln_stk_estk',TEXT, nullable=False),
+    Column('dpstk_prc_ostk',TEXT, nullable=False) ,
+    Column('dpstk_prc_estk',TEXT, nullable=False),
+    Column('dppln_prc_ostk',TEXT, nullable=False) ,
+    Column('dppln_prc_estk',TEXT, nullable=False),
+    Column('dpprpd_bgd',TEXT, nullable=False) ,
+    Column('dpprpd_edd',TEXT, nullable=False),
+    Column('dp_pp',TEXT, nullable=False),
+    Column('dp_m_mkt',TEXT, nullable=False),
+    Column('dp_m_ovtm',TEXT, nullable=False),
+    Column('dp_m_otc',TEXT, nullable=False),
+    Column('dp_m_etc',TEXT, nullable=False),
+    Column('cs_iv_bk',TEXT, nullable=False),
+    Column('aq_wtn_div_ostk',TEXT, nullable=False),
+    Column('aq_wtn_div_ostk_rt',TEXT, nullable=False),
+    Column('aq_wtn_div_estk',TEXT, nullable=False),
+    Column('aq_wtn_div_estk_rt',TEXT, nullable=False),
+    Column('eaq_ostk',TEXT, nullable=False),
+    Column('eaq_ostk_rt',TEXT, nullable=False),
+    Column('eaq_estk',TEXT, nullable=False),
+    Column('eaq_estk_rt',TEXT, nullable=False),
+    Column('dp_dd',TEXT, nullable=False),
+    Column('od_a_at_t',TEXT, nullable=False),
+    Column('od_a_at_b',TEXT, nullable=False),
+    Column('adt_a_atn',TEXT, nullable=False),
+    Column('d1_slodlm_ostk',TEXT, nullable=False),
+    Column('d1_slodlm_estk',TEXT, nullable=False),
+    )
+tbStockDispositionInfo.create(conn.engine,checkfirst=True)
+
+@stockDispositionRouter.get("/tb_stock_disposition/")
+async def paidIncreaseMain():
+    return "stockDisposition runs successfully"
+
+@stockDispositionRouter.get("/tb_stock_disposition/selectStockDispositionInfoByDay/{startDate}/{endDate}") #json 리스트로 줌
+async def selectStockDispositionInfoByDay(startDate: int,endDate: int):
+    session = conn.sessionmaker()
+    #rcept_no_str = cast(paidIncreaseInfo.rcept_no, String)
+    #resli = session.query(paidIncreaseInfo).filter(rcept_no_str.like(f'{day}%')).all()
+    resli = session.query(tbStockDispositionInfo).filter(tbStockDispositionInfo.rcept_no >= startDate, tbStockDispositionInfo.rcept_no <= endDate).all()
+    if resli == None or len(resli) == 0: return {'code': 1}
+    res = {}
+    res['list'] = resli
+    res['code'] = 0
+    session.close()
+    return res
+
+@stockDispositionRouter.post("/tb_stock_disposition/setStockDispositionInfo")
+async def setStockDispositionInfo(body: Request):
+    session = conn.sessionmaker()
+    json_str = await body.json()
+    logger.info(json_str)
+    try:
+        json_str['rcept_no'] = int(json_str['rcept_no'])
+        stmt = insert(tbStockDispositionInfo).values(json_str)
+        session.execute(stmt)
+        session.commit()
+        logger.debug("삽입완료")
+    except sqlalchemy.exc.IntegrityError as e:
+        logger.debug("삽입 중 오류, 이미 들어가있음 {e}", e=e, exc_info=True)
+        session.rollback()
+    except Exception as e:
+        logger.debug("삽입 중 오류 {e}",e=e,exc_info=True)
+        session.rollback()
+    session.close()
+    return {'code': 0}
--- FastApiCore/openDartSvc/stockDispositionSvc.py
+++ FastApiCore/openDartSvc/stockDispositionSvc.py
@@ -2,7 +2,7 @@ import json
 import sys,os
 sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__)))) #상위 경로를 현재 경로에 넣어 declaration 파일 임포트 가능
 from declaration import crtfc_key, dbUrl, sendDiscordMessage
-from eventPushSvcIntf import CmnEventPushSvcIntf
+from openDartSvc.eventPushSvcIntf import CmnEventPushSvcIntf
 '''
 사용 파라미터 : 회사코드, 날짜
 주요 내용 조립해서 푸시 서버에 보내줌
@@ -15,16 +15,17 @@ class StockDispositionIntf(CmnEventPushSvcIntf):
     def __init__(self):
         self.requestUrl = "https://opendart.fss.or.kr/api/piicDecsn.json"  # 자기주식 처분결정 api url
     def pushMessage(self,msgInfo: dict):
-        corpName = msgInfo['corpName']
-        rceptDt = msgInfo['rceptDt']
-        reportNm = msgInfo['reportNm']
-        corpCode = msgInfo['corpCode']
+        corpName = msgInfo['corp_name']
+        rceptDt = msgInfo['rcept_dt']
+        reportNm = msgInfo['report_nm']
+        corpCode = msgInfo['corp_code']
         logger.debug(f"{corpName} {reportNm} {rceptDt} {corpCode}")
         param = {"crtfc_key" : crtfc_key, "corp_code" : corpCode, "bgn_de" : rceptDt, "end_de" : rceptDt}
         res = requests.get(self.requestUrl,params=param).json()
         logger.debug(res)
         if res['status'] == '013':
             logger.debug("아무 이벤트 없음")
+            super().pushMessage(msgInfo)
             return
         if res['status'] != '000':
             logger.debug("오류 발생")
@@ -32,11 +33,16 @@ class StockDispositionIntf(CmnEventPushSvcIntf):
         #유상증자와 마찬가지로 기재정정이라고 생각
         res = res['list'][0]
         #해당 데이터 db에 저장
-        requests.post(dbUrl + "/tb_paid_increase/setCorpInfo",json=res)
-        msg = f"자기주식처분 결정 \n 회사명 : {res['corp_name']} \n 발행주식 수 : {res['nstk_ostk_cnt']} \n 주당액면가 : {res['fv_ps']}" \
-              f"\n 증자방식 : {res['ic_mthn']}"
+        self.saveDB(res)
+        #requests.post(dbUrl + "/tb_paid_increase/setCorpInfo",json=res)
+        msg = f"자기주식처분 결정 \n회사명 : {res['corp_name']} \n발행주식 수 : {res['nstk_ostk_cnt']} \n주당액면가 : {res['fv_ps']}" \
+              f"\n증자방식 : {res['ic_mthn']} \n접수번호 : {res['rcept_no']}"
         logger.debug(msg)
         #여기에 푸시 보내는 기능 적재예정
         res = sendDiscordMessage(msg)
         logger.debug("메세지 보냄 " + json.dumps(res))
+    def saveDB(self,msgInfo: dict):
+        res = requests.post(dbUrl + "/tb_stock_disposition/insertDispositionInfo", json=msgInfo)
+        res = res.json()
+        logger.debug(res)
 
