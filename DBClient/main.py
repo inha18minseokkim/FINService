8e2042dcc9bace1edaecfe82f375e8bfc862597f
bjm7701 <bjm7701@naver.com>
2023-03-01 23:37:53 +0900

    자기주식처분결정, 일반공시 라우터 추가

--- DBClient/main.py
+++ DBClient/main.py
@@ -3,6 +3,8 @@ from databaseCmn import engineConn
 from corpCode.corpCodeMain import corpCodeRouter
 from convertibleBond.cbMain import cbRouter
 from paidIncrease.paidIncreaseMain import paidIncreaseRouter
+from stockDisposition.stockDispositionMain import stockDispositionRouter
+from corpCmnAnn.corpCmnAnnMain import corpCmnAnnRouter
 app = FastAPI()
 conn = engineConn()
 session = conn.sessionmaker()
@@ -10,6 +12,8 @@ session = conn.sessionmaker()
 app.include_router(corpCodeRouter)
 app.include_router(cbRouter)
 app.include_router(paidIncreaseRouter)
+app.include_router(stockDispositionRouter)
+app.include_router(corpCmnAnnRouter)
 #meta_data = MetaData(bind=conn,reflect=True)
 #finServiceTable = meta_data.tables['TB_CORP_CODE']
 
