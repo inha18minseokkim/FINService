2595d5da9a3ab025fb3b3a97c62d1f86e7a1ed14
bjm7701 <bjm7701@naver.com>
2023-03-11 18:22:50 +0900

    아파트 경쟁률 조회서비스작성중

---
+++ RealEstateCompetitionRate/apartment/dbModel.py
@@ -0,0 +1,93 @@
+from loguru import logger
+from sqlalchemy import Column, TEXT, INT, BIGINT, UniqueConstraint, Integer
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+class TB_APT_COMPETITION_RATE(Base):
+    __tablename__ = "TB_APT_COMPETITION_RATE"
+    HOUSE_MANAGE_NO = Column(INT, autoincrement=True, primary_key=True)
+    PBLANC_NO = Column(INT, nullable=True)
+    MODEL_NO = Column(TEXT, nullable=True)
+    HOUSE_TY = Column(TEXT, nullable=True)
+    SUPLY_HSHLDCO = Column(INT, nullable=True)
+    SUBSCRPT_RANK_CODE = Column(INT, nullable=True)
+    RESIDE_SECD = Column(TEXT, nullable=True)
+    RESIDE_SENM = Column(TEXT, nullable=True)
+    REQ_CNT = Column(TEXT, nullable=True)
+    CMPET_RATE = Column(TEXT, nullable=True)
+    LWET_SCORE = Column(INT, nullable=True)
+    TOP_SCORE = Column(INT, nullable=True)
+    AVRG_SCORE = Column(INT, nullable=True)
+    
+    def __init__(self, jsonData):
+        self.HOUSE_MANAGE_NO = jsonData.get('HOUSE_MANAGE_NO')
+        self.PBLANC_NO = jsonData.get('PBLANC_NO')
+        self.MODEL_NO = jsonData.get('MODEL_NO')
+        self.HOUSE_TY = jsonData.get('HOUSE_TY')
+        self.SUPLY_HSHLDCO = jsonData.get('SUPLY_HSHLDCO')
+        self.SUBSCRPT_RANK_CODE = jsonData.get('SUBSCRPT_RANK_CODE')
+        self.RESIDE_SECD = jsonData.get('RESIDE_SECD')
+        self.RESIDE_SENM = jsonData.get('RESIDE_SENM')
+        self.REQ_CNT = jsonData.get('REQ_CNT')
+        self.CMPET_RATE = jsonData.get('CMPET_RATE')
+        self.LWET_SCORE = jsonData.get('LWET_SCORE')
+        self.TOP_SCORE = jsonData.get('TOP_SCORE')
+        self.AVRG_SCORE = jsonData.get('AVRG_SCORE')
+        
+    def to_dict(self):
+        return {
+            'HOUSE_MANAGE_NO' : self.HOUSE_MANAGE_NO,
+            'PBLANC_NO' : self.PBLANC_NO,
+            'MODEL_NO' : self.MODEL_NO,
+            'HOUSE_TY' : self.HOUSE_TY,
+            'SUPLY_HSHLDCO' : self.SUPLY_HSHLDCO,
+            'SUBSCRPT_RANK_CODE' : self.SUBSCRPT_RANK_CODE,
+            'RESIDE_SECD' : self.RESIDE_SECD,
+            'RESIDE_SENM' : self.RESIDE_SENM,
+            'REQ_CNT' : self.REQ_CNT,
+            'CMPET_RATE' : self.CMPET_RATE,
+            'LWET_SCORE' : self.LWET_SCORE,
+            'TOP_SCORE' : self.TOP_SCORE,
+            'AVRG_SCORE' : self.AVRG_SCORE
+        }
+    def getMsg(self):
+        curdict = self.to_dict()
+        logger.debug(curdict)
+        resMsg = ""
+        for k,v in curdict.items():
+            resMsg += infoDict[k]
+            resMsg += " : "
+            resMsg += f"{v}"
+            resMsg += "\n"
+        return resMsg
+infoDict = {
+'HOUSE_MANAGE_NO' : '주택관리번호',
+
+'PBLANC_NO' : '공고번호',
+
+'HOUSE_NM' : '주택명',
+
+'MODEL_NO'	:'모델번호',
+
+'HOUSE_TY'	: '주택형',
+
+'SUPLY_HSHLDCO' :	'공급세대수',
+
+'SUBSCRPT_RANK_CODE' :	'순위',
+
+'RESIDE_SECD' :	'거주코드 (01: 해당지역, 02: 기타지역, 03: 기타경기)',
+
+'RESIDE_SENM' :	'거주지역',
+
+'REQ_CNT'	: '접수건수',
+
+'CMPET_RATE'    : '경쟁률',
+
+'LWET_SCORE' :	'최저당첨가점',
+
+'TOP_SCORE':	'최고당첨가점',
+
+'AVRG_SCORE' :	'평균당첨가점'
+}
