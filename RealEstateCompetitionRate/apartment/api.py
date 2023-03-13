746ee9cdf865db8413d2361ca26a373f03fe3223
bjm7701 <bjm7701@naver.com>
2023-03-13 20:23:00 +0900

    경쟁률을 각각 세부영역(아파트,오피스텔 등)으로 분화함에 따라 라우터로 분리

---
+++ RealEstateCompetitionRate/apartment/api.py
@@ -0,0 +1,33 @@
+from fastapi import APIRouter
+from apartment import dbClient,mainSvc,dbModel
+from loguru import logger
+import requests
+
+apartmentRouter = APIRouter(tags=["아파트"])
+
+@apartmentRouter.get("/dailyApartmentData")
+async def dailyApartmentData(startDate: str, endDate: str):
+    rqstUrl = f"http://172.28.166.206:32221/getHouseManageNmByDay?startDate={startDate}&endDate={endDate}"
+    res = requests.get(rqstUrl)
+    if res.status_code != 200:
+        return {'code' : 1}
+    res = res.json()
+    logger.debug(res)
+    portalUrl = "https://api.odcloud.kr/api/ApplyhomeInfoCmpetRtSvc/v1/getAPTLttotPblancCmpet?page=1&perPage=1000"
+    dbClient.createTable()
+    for num in res['list']:
+        competition:list[dbModel.aptCompetitionRate] = mainSvc.executeApi(num, 1000)
+        for ele in competition:
+            logger.debug(ele.to_dict())
+            dbClient.saveDB(ele)
+
+    return res
+@apartmentRouter.get("/getApartmentCmpByHouseManageNm")
+async def getApartmentCmpByHouseManageNm(houseManageNm: str):
+    res:list[dbModel.aptCompetitionRate] = dbClient.selectDataByHouseManageNm(houseManageNm)
+    resarr = []
+    for ele in res:
+        resarr.append(ele.to_dict())
+    return {"list": resarr}
+
+
